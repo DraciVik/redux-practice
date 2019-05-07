5ad06ca7b4de8677f1755711a56590ae3e238a0e
DraciVik <viktor.stojanov90@gmail.com>
2019-05-07 22:30:23 +0200

    Added connect and mapStateToProps

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import index from "./js/index";
+import React from "react";
+import { render } from "render-dom";
+import { Provider } from "react-redux";
+import store from "./js/store/index";
+import App from "./js/components/App.jsx";
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
