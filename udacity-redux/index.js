71b4821216fe6885e3c8b336e7163a718625eefc
DraciVik <viktor.stojanov90@gmail.com>
2019-06-07 00:45:40 +0200

    Added a best practice for action types by converting them to constant variables

--- udacity-redux/index.js
+++ udacity-redux/index.js
@@ -31,13 +31,20 @@ function createStore(reducer) {
 }
 
 // App Code
+
+const ADD_TODO = 'ADD_TODO';
+const REMOVE_TODO = 'REMOVE_TODO';
+const TOGGLE_TODO = 'TOGGLE_TODO';
+const ADD_GOAL = 'ADD_GOAL';
+const REMOVE_GOAL = 'REMOVE_GOAL';
+
 function todos(state = [], action) {
         switch (action.type) {
-                case 'ADD_TODO':
+                case ADD_TODO:
                         return state.concat([action.todo]);
-                case 'REMOVE_TODO':
+                case REMOVE_TODO:
                         return state.filter(todo => todo.id !== action.id);
-                case 'TOGGLE_TODO':
+                case TOGGLE_TODO:
                         return state.map(todo =>
                                 todo.id !== action.id ? todo : Object.assign({}, todo, { complete: !todo.complete })
                         );
@@ -48,9 +55,9 @@ function todos(state = [], action) {
 
 function goals(state = [], action) {
         switch (action.type) {
-                case 'ADD_GOAL':
+                case ADD_GOAL:
                         return state.concat([action.goal]);
-                case 'REMOVE_GOAL':
+                case REMOVE_GOAL:
                         return state.filter(goal => goal.id !== action.id);
                 default:
                         return state;
@@ -73,10 +80,59 @@ store.subscribe(() => {
 });
 
 store.dispatch({
-        type: 'ADD_TODO',
+        type: ADD_TODO,
         todo: {
                 id: 0,
-                name: 'Learn Redux',
+                name: 'Walk the dog',
                 complete: false,
         },
+});
+
+store.dispatch({
+        type: ADD_TODO,
+        todo: {
+                id: 1,
+                name: 'Wash the car',
+                complete: false,
+        },
+});
+
+store.dispatch({
+        type: ADD_TODO,
+        todo: {
+                id: 2,
+                name: 'Go to the gym',
+                complete: true,
+        },
+});
+
+store.dispatch({
+        type: REMOVE_TODO,
+        id: 1,
+});
+
+store.dispatch({
+        type: TOGGLE_TODO,
+        id: 0,
+});
+
+store.dispatch({
+        type: ADD_GOAL,
+        goal: {
+                id: 0,
+                name: 'Learn Redux',
+        },
+});
+
+store.dispatch({
+        type: ADD_GOAL,
+        goal: {
+                id: 1,
+                name: 'Lose 20 pounds',
+        },
+});
+
+store.dispatch({
+        type: REMOVE_GOAL,
+        id: 0,
 });
